93a5657b0011c9d5f5b535600e7186926e50ed96
Pribadiku1 <emailkushusresult@gmail.com>
2025-08-13 12:02:01 +0700

    Add files via upload

---
+++ api/random-image.js
@@ -0,0 +1,15 @@
+const https = require('https');
+
+const imagesList = require('../lists/example-images-list.json');
+const images = imagesList.images;
+  
+  module.exports = (req, res) => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    console.log("Image: " + randomImage);
+  
+    https.get(randomImage, (response) => {
+      res.setHeader('Content-Type', 'image/jpeg');
+      response.pipe(res);
+    });
+  };
